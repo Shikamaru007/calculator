0fd692c6b8e11cea07cde9aec41a422ccf3cee5e
froZin <bolutolu12@gmail.com>
2024-09-09 13:47:45 +0100

    completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 
+const display = document.getElementById("display")
+const clear = document.getElementById("clear")
+
+function btn(input) {
+    display.value += input;
+}
+
+clear.onclick = function() {
+    display.value = "";
+}
+
+function calculate(){
+    
+    try {
+        display.value = eval(display.value);
+    }
+    catch(error) {
+        display.value = "eRRoR"
+    }
+}
